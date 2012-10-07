0fc36ae23c24f7f66782ae4184b409cee58cc908
Nadav Cohen <nadavcoh@gmail.com>
2012-10-07 14:10:04 +0200

    Added log4j initialization and example

--- services/src/main/java/com/mycompany/services/GreetingServiceImpl.java
+++ services/src/main/java/com/mycompany/services/GreetingServiceImpl.java
@@ -1,5 +1,6 @@
 package com.mycompany.services;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
@@ -7,11 +8,18 @@ import javax.inject.Inject;
 @Service
 public class GreetingServiceImpl implements GreetingService {
 
+    private Logger logger = Logger.getLogger(getClass());
+
     @Inject
     private RandomWordService randomWordService;
 
     public String greetServer(String name) throws IllegalArgumentException {
         String randomHello = randomWordService.readRandomWordFor("hello");
-        return String.format("%s, %s!", randomHello, name);
+        String result = String.format("%s, %s!", randomHello, name);
+
+        // Example usage of Log4j
+        logger.info("Returning from greetServer() with: " + result);
+
+        return result;
     }
 }
